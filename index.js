93ec36289b7c881116921fb6ad69a1443cb2498f
Mateus Nobre <mateusnobref@gmail.com>
2023-09-03 15:19:28 -0300

    express adicionado

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const { Client } = require("@notionhq/client")
+const express = require('express')
+const app = express()
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 const NOTION_TOKEN = process.env.NOTION_TOKEN;
 const DATABASE_ID = process.env.DATABASE_ID;
 
-const bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
+const bot = new TelegramBot(TELEGRAM_TOKEN, {
+    polling: true, 
+});
 
 const notion = new Client({
-  auth: NOTION_TOKEN
+  auth: NOTION_TOKEN,
 })
 
-bot.onText(/\/s (.+)/, async  (msg, match) => {
+bot.onText(/\/s (.+)/, async  (msg, match) => { 
     console.log("Nova mensagem de criação de item recebida!")
     const chatId = msg.chat.id;
     const resp = match[1];
@@ -24,6 +28,12 @@ bot.onText(/\/s (.+)/, async  (msg, match) => {
 
     bot.sendMessage(chatId, "Item Criado!");
 });
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+  
+app.listen(process.env.PORT || 3000)
   
 console.log("Server Iniciado!")
 

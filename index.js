26c44d4c0f9b685a869449e9bf6ab7ce49397ec7
Mateus Nobre <mateusnobref@gmail.com>
2023-09-03 14:31:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const TelegramBot = require('node-telegram-bot-api');
+const { Client } = require("@notionhq/client")
+
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+const NOTION_TOKEN = process.env.NOTION_TOKEN;
+const DATABASE_ID = process.env.DATABASE_ID;
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
+
+const notion = new Client({
+  auth: NOTION_TOKEN
+})
+
+bot.onText(/\/s (.+)/, async  (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1];
+
+    await notion.pages.create({
+        parent: { database_id: DATABASE_ID },
+        properties: { Name: {title: [ { text: {content: resp }}]},
+    }})
+
+    bot.sendMessage(chatId, "Item Criado!");
+  });
+  
